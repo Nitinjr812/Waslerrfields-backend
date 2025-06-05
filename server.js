1dcaf2fa3dba67329b02b6693ef8fb4ab8cec5d6
Nitinjr812 <nitinjr812@gmail.com>
2025-06-05 13:28:41 +0530

    backendt

--- server.js
+++ server.js
@@ -27,15 +27,20 @@ app.use('/api/auth', auth);
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
-  PORT,
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
-  )
+    PORT,
+    console.log(
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    )
 );
+app.get("/", (req, res) => {
+    res.json({
+        status: true
+    })
+})
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`);
-  // Close server & exit process
-  server.close(() => process.exit(1));
+    console.log(`Error: ${err.message}`);
+    // Close server & exit process
+    server.close(() => process.exit(1));
 });
