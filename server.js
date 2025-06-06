1dfb23e2e2f26fce1e2070418449a892c2b9f11a
Nitinjr812 <nitinjr812@gmail.com>
2025-06-06 23:46:04 +0530

    wapas sahi?p

--- server.js
+++ server.js
@@ -1,50 +1,46 @@
-    const express = require('express');
-    const mongoose = require('mongoose');
-    const dotenv = require('dotenv');
-    const cors = require('cors');
-    const connectDB = require('./config/db');
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
 
-    // Load env vars
-    dotenv.config();
+// Load env vars
+dotenv.config();
 
-    // Connect to database
-    connectDB();
+// Connect to database
+connectDB();
 
-    // Route files
-    const auth = require('./routes/auth');
-    const music = require('./routes/music');
+// Route files
+const auth = require('./routes/auth');
 
-    const app = express();
+const app = express();
 
-    // Body parser
-    app.use(express.json());
+// Body parser
+app.use(express.json());
 
-    // Enable CORS
-    app.use(cors());
+// Enable CORS
+app.use(cors());
 
-    // Mount routers
-    app.use('/api/auth', auth);
-    app.use('/api/music', music);
+// Mount routers
+app.use('/api/auth', auth);
 
-    const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-    const server = app.listen(
-        PORT,
-        console.log(
-            `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
-        )
-    );
+const server = app.listen(
+    PORT,
+    console.log(
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    )
+);
+app.get("/", (req, res) => {
+    res.json({
+        status: true
+    })
+})
 
-    app.get("/", (req, res) => {
-        res.json({
-            status: true,
-            message: "Music API Server is running!"
-        })
-    });
-
-    // Handle unhandled promise rejections
-    process.on('unhandledRejection', (err, promise) => {
-        console.log(`Error: ${err.message}`);
-        // Close server & exit process
-        server.close(() => process.exit(1));
-    });
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    // Close server & exit process
+    server.close(() => process.exit(1));
+});
--- utils/errorResponse.js
+++ utils/errorResponse.js
@@ -1,10 +1,7 @@
-// utils/errorResponse.js
 class ErrorResponse extends Error {
   constructor(message, statusCode) {
     super(message);
     this.statusCode = statusCode;
-
-    Error.captureStackTrace(this, this.constructor);
   }
 }
 
