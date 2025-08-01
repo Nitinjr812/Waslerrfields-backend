418fbd0952252ce775a28f197488359d65b92f65
Nitinjr812 <nitinjr812@gmail.com>
2025-08-01 12:46:09 +0530

    Your commit message here

--- config/r2.js
+++ config/r2.js
@@ -1,11 +1,11 @@
-const AWS = require("aws-sdk");
+const AWS = require('aws-sdk');
 
 const s3 = new AWS.S3({
   endpoint: `https://${process.env.CLOUDFLARE_ACCOUNT_ID}.r2.cloudflarestorage.com`,
   accessKeyId: process.env.R2_ACCESS_KEY,
   secretAccessKey: process.env.R2_SECRET_KEY,
-  signatureVersion: "v4",
-  region: "auto" // or specific region if you want, but auto works fine
+  signatureVersion: 'v4',
+  region: 'auto'
 });
 
 module.exports = s3;
